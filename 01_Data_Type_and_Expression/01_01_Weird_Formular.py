936923d694eaa8c037512d50e7d86e12abb4e166
SKT-Sukatat <82657530+SKT-Sukatat@users.noreply.github.com>
2024-06-30 09:59:08 +0700

    First_Problem

---
+++ 01_Data_Type_and_Expression/01_01_Weird_Formular.py
@@ -0,0 +1,8 @@
+import math
+
+numerator  = math.pi - (math.factorial(10)/8**8) + (math.log(9.7)**(7/math.sqrt(71) - (math.sin(math.radians(40)))))
+denominator = (1.2)**(2.3**(1/3))
+
+result = round(numerator/denominator, 6)
+
+print(result)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Python_Labbook_Chula_2024
+
+- This Respositary Collect and Maintain all of the Fundamental Problems (00:01 - 12:08)
