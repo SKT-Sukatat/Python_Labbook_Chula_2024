1979e72c93de34a199d8b61edfd4f107437e2528
SKT-Sukatat <82657530+SKT-Sukatat@users.noreply.github.com>
2024-07-02 16:35:42 +0700

    Add comment to Pb 02-06

--- 02_Basic_String_and_List/02_06_Summation_of_Vectors.py
+++ 02_Basic_String_and_List/02_06_Summation_of_Vectors.py
@@ -9,11 +9,12 @@ v_list = list(v[1:-1].split(", "))
 u1, u2, u3 = float(u_list[0]), float(u_list[1]), float(u_list[2])
 v1, v2, v3 = float(v_list[0]), float(v_list[1]), float(v_list[2])
 
+# Summation of each elements of vector
 x1 = u1 + v1
 x2 = u2 + v2
 x3 = u3 + v3
 
-# Summation of Vectors
+# Display result
 print(type(u_list[0]))
 print( '[' + str(u1) + ', '+ str(u2) + ', ' + str(u3) + '] + ' + \
       '[' + str(v1) + ', '+ str(v2) + ', ' + str(v3) + '] = [' +  \
