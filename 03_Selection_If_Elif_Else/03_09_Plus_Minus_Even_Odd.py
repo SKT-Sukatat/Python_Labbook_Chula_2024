dc0b4b2ef0c4a3c81aa3024d93068d880b30d80f
SKT-Sukatat <82657530+SKT-Sukatat@users.noreply.github.com>
2024-07-03 23:35:12 +0700

    Add Pb 03_09

---
+++ 03_Selection_If_Elif_Else/03_09_Plus_Minus_Even_Odd.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+if n>0:
+    print("positive")
+elif n<0:
+    print("negetive")
+else:
+    print("zero")
+
+if n % 2 == 0:
+    print("even")
+elif n % 2 == 1:
+    print("odd")
