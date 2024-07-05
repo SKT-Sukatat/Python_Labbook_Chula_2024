6f7e883a244686a7eb852b56265dc5ef5999910d
SKT-Sukatat <82657530+SKT-Sukatat@users.noreply.github.com>
2024-07-05 21:17:47 +0700

    Add Pb_04_04

--- 04_Repetition_For_While/04_04_bisection.py
+++ 04_Repetition_For_While/04_04_bisection.py
@@ -3,12 +3,11 @@ a = float(input())
 l = 0; u = a
 x = (l+u)/2
 
-while abs(a-10**x) <= (10**(-10))*max(a,10**x):
+while abs(a-10**x) >= (10**(-10))*max(a,10**x):
     x = (l+u)/2
     if 10**x > a:
         u = x
     elif 10**x < a:
         l =x
-    print(x)
 
 print(round(x, 6))
