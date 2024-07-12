e9e0d1ca9aa97323478626913a361b77c90e45eb
SKT-Sukatat <82657530+SKT-Sukatat@users.noreply.github.com>
2024-07-12 20:16:50 +0700

    Add Pb_05_08

---
+++ 05_List_Processing/05_08_Adjust_Grade_2.py
@@ -0,0 +1,34 @@
+all_grades = ['F', 'D', 'D+', 'C', 'C+', 'B', 'B+', 'A']
+grades = []
+ids = []
+uids = []
+
+while True:
+    data = input()
+    if data == 'q':
+        choosen_uid = [x for x in input().split()]
+        for i in range(len(choosen_uid)):
+            uids.append(choosen_uid[i])
+        break
+    else:    
+        id, grade = data.split()
+        ids.append(id)
+        grades.append(grade)
+
+for uid in uids:
+    if uid in ids:
+        index = ids.index(uid)
+        grade = grades[index]
+        new_grade = all_grades[all_grades.index(grade) + 1]
+        grades[index] = new_grade
+
+results = []
+
+for i in range(len(ids)):
+    result = str(ids[i]) + " "+grades[i]
+    results.append(result)
+
+results.sort(key = lambda x: x.split()[0])
+
+for result in results:
+    print(result)
